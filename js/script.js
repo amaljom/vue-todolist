2673ddb8cff25380282eacb2cb56f235f084baf9
amaljom <amaljom99@gmail.com>
2022-07-06 14:17:00 +0200

    aggiunti dinamicamente gli elementi della lista

--- js/script.js
+++ js/script.js
@@ -3,6 +3,27 @@ const app = new Vue(
     el: "#root",
 
     data:{
-
+        todos:[
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare il bucato',
+                done: false
+            },
+            {
+                text: 'comprare il pane',
+                done: true
+            },
+            {
+                text: 'comprare il latte',
+                done: true
+            }
+        ]
     },
 });
